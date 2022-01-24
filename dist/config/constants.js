21cf20f2a6892193ca2cd258461356a1a292ae3e
Salomão Cruz <82367055+shl0mo@users.noreply.github.com>
2022-01-24 03:50:56 -0400

    Delete dist directory

--- dist/config/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    HOST: 'http://localhost',
-    PORTA: '3001',
-    URL_API: "http://localhost:3001",
-    MONGO_CONN: "mongodb+srv://user:pass@encurtador-url.ghsnr.mongodb.net/encurtador-url?retryWrites=true&w=majority"
-};
